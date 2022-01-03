1f20b27f9323f3807c0f3a2ece7e742930ba1e83
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-03 13:30:07 +0000

    add constants

--- day-2/2-1-constants.go
+++ day-2/2-1-constants.go
@@ -2,6 +2,24 @@ package main
 
 import ("fmt")
 
+const age = 20
+var number int = 42
+
+const(
+luckyNumber int = 3
+pi = 3.14
+name = "gokhan"
+)
+
 func main(){
-  fmt.Println("new year")
+  number = 41
+  fmt.Println(number)
+  //if i tried to change age as 21. it will be error.
+  fmt.Println(age)
+
+  fmt.Println(luckyNumber)
+  fmt.Println(pi)
+  fmt.Println(name)
+
+
 }
