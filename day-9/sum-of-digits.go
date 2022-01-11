0492d31233ebe5918f90866de90a3a148dd9f1b8
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-11 18:24:38 +0000

    sum of digits update

--- day-9/sum-of-digits.go
+++ day-9/sum-of-digits.go
@@ -8,12 +8,12 @@ func main() {
 	fmt.Scan(&T)
 
 	for T > 0 {
-		var number int
-		fmt.Scan(&number) //769
+		var N int
+		fmt.Scan(&N) //769
 		var addition int
-		for number > 0 {
-			addition = addition + number%10 //Modulus ex: 769 -> 9
-			number = number / 10            //Division ex: 847 -> 84
+		for N > 0 {
+			addition = addition + N%10 //Modulus ex: 769 -> 9
+			N = N / 10            //Division ex: 847 -> 84
 		}
 		fmt.Println(addition)
 		T = T - 1 //or t--
