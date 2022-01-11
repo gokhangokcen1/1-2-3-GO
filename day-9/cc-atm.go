096697c259e3e918f8b59347e10392c56a29b1bb
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-11 17:57:53 +0000

    cc-atm update

--- day-9/cc-atm.go
+++ day-9/cc-atm.go
@@ -4,16 +4,14 @@ import "fmt"
 
 func main() {
 	var withdraw int
-	fmt.Print("Withdraw : ")
-	fmt.Scanln(&withdraw)
+	fmt.Scan(&withdraw)
 
 	var balance float64
-	fmt.Print("Balance : ")
-	fmt.Scanln(&balance)
+	fmt.Scan(&balance)
 
 	if (withdraw%5 == 0) && (balance >= float64(withdraw)+0.5) {
-		result := balance - float64(withdraw) - 0.5
-		fmt.Printf("%v \t %v \t %v \n", withdraw, balance, result)
+		fmt.Println(balance - float64(withdraw) - 0.5)
+		
 
 	} else {
 		fmt.Println(balance)
