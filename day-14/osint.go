c0dd4ecf3885b3a9f94274325d92bcab18c64cd4
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-19 15:13:36 +0300

    Update osint.go

--- day-14/osint.go
+++ day-14/osint.go
@@ -1,74 +1,74 @@
-
 package main
 
 import (
 	"fmt"
 	"net/http"
-  "github.com/fatih/color"
+
+	"github.com/fatih/color"
 )
-  func main(){
 
-    var name string
-    fmt.Print("kullanıcı adı : ")
-    fmt.Scanln(&name)
-    fmt.Println()
-    tiktok(name)
-    vsco(name)
-    instagram(name)
-    twitter(name)
- 
-  }
+func main() {
 
-  func tiktok(name string){
-    url := "https://tiktok.com/@"+name
-    color.Magenta("[?] %v için TikTok taraması yapılıyor.\n",name)
-    res,_ := http.Get(url)
+	var name string
+	fmt.Print("kullanıcı adı : ")
+	fmt.Scanln(&name)
+	fmt.Println()
+	tiktok(name)
+	vsco(name)
+	instagram(name)
+	twitter(name)
 
-    if res.StatusCode !=200{
-      color.Red("[-] Hesap bulunamadı")
-      return
-    }else{
-      color.Cyan("[+] Hesap bulundu")
-    }
-  }
+}
 
-  func vsco(name string){
-    
-    url := "https://vsco.co/"+name+"/gallery"
-    color.Magenta("[?] %v için VSCO taraması yapılıyor.\n",name)
-    res,_ := http.Get(url)
+func tiktok(name string) {
+	url := "https://tiktok.com/@" + name
+	color.Magenta("[?] %v için TikTok taraması yapılıyor.\n", name)
+	res, _ := http.Get(url)
 
-    if res.StatusCode !=200{
-      color.Red("[-] Hesap bulunamadı")
-      return
-    }else{
-      color.Cyan("[+] Hesap bulundu")
-    }
-    }
+	if res.StatusCode != 200 {
+		color.Red("[-] Hesap bulunamadı")
+		return
+	} else {
+		color.Cyan("[+] Hesap bulundu")
+	}
+}
 
+func vsco(name string) {
 
-    func instagram(name string){
-    url := "https://instagram.com/"+name
-    color.Magenta("[?] %v için Instagram taraması yapılıyor.\n",name)
-    res,_ := http.Get(url)
+	url := "https://vsco.co/" + name + "/gallery"
+	color.Magenta("[?] %v için VSCO taraması yapılıyor.\n", name)
+	res, _ := http.Get(url)
 
-    if res.StatusCode !=200{
-      color.Red("[-] Hesap bulunamadı")
-      return
-    }else{
-      color.Cyan("[+] Hesap bulundu")
-    }
-    }
+	if res.StatusCode != 200 {
+		color.Red("[-] Hesap bulunamadı")
+		return
+	} else {
+		color.Cyan("[+] Hesap bulundu")
+	}
+}
 
-    func twitter(name string){
-    url := "https://twitter.com/"+name
-    color.Magenta("[?] %v için Twitter taraması yapılıyor.\n",name)
-    res,_ := http.Get(url)
+func instagram(name string) {
+	url := "https://instagram.com/" + name
+	color.Magenta("[?] %v için Instagram taraması yapılıyor.\n", name)
+	res, _ := http.Get(url)
 
-    if res.StatusCode !=200{
-      color.Red("[-] Hesap bulunamadı")
-      return
-    }else{
-      color.Cyan("[+] Hesap bulundu")
-    }
-    }
+	if res.StatusCode != 200 {
+		color.Red("[-] Hesap bulunamadı")
+		return
+	} else {
+		color.Cyan("[+] Hesap bulundu")
+	}
+}
+
+func twitter(name string) {
+	url := "https://twitter.com/" + name
+	color.Magenta("[?] %v için Twitter taraması yapılıyor.\n", name)
+	res, _ := http.Get(url)
+
+	if res.StatusCode != 200 {
+		color.Red("[-] Hesap bulunamadı")
+		return
+	} else {
+		color.Cyan("[+] Hesap bulundu")
+	}
+}
