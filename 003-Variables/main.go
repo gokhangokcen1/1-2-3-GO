a1087e10d2b9c522cc78c20cbf5ba0477f10f508
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-24 21:29:02 +0300

    Update and rename day-01/1-2-variables.go to 003-Variables/main.go

--- 003-Variables/main.go
+++ 003-Variables/main.go
@@ -7,6 +7,27 @@ package main
     bool- stores values with two states: true or false
 
 */
+
+/* 
+BASIC TYPES
+bool
+
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // alias for uint8
+
+rune // alias for int32
+     // represents a Unicode code point
+
+float32 float64
+
+complex64 complex128
+*/
+
+
 import (
   "fmt"
 )
@@ -66,5 +87,9 @@ func main() {
   fmt.Println(o)
   fmt.Println(t)
   fmt.Println(f)
+    
+  integer := 42
+  float = float64(integer) 
+  u := uint(float)
 
 }
