3f53c1e055c4b77a2ae2aab78603ca6400877230
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-24 21:40:28 +0300

    Update main.go

--- 007-Switch/main.go
+++ 007-Switch/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "time"
+)
 
 func main(){
   day := 4
@@ -34,6 +37,19 @@ func main(){
     default:
       fmt.Printf("%v is not here\n", name)
   }
+  
+  fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
 
 
 }
