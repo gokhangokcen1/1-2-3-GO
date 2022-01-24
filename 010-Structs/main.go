a01807830c3b3bfb9a4b4f26eeb93fd85c66a79a
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-01-24 21:46:12 +0300

    Update and rename day-08/1-struct.go to 010-Structs/main.go

--- 010-Structs/main.go
+++ 010-Structs/main.go
@@ -9,9 +9,24 @@ type Students struct{
   classNo int
 }
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main(){
+  
+v := Vertex{1, 2}
+v.X = 4
+fmt.Println(v.X)  
+  
+p := &v
+p.X = 1e9
+fmt.Println(v)
+  
 var student1 Students
 var student2 Students
+ 
 
 student1.name = "Gökhan"
 student1.surname = "Gökcen"
